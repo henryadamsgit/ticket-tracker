e8d9712d3b69fbbab7c8dbca7e0788d981d1e60a
Henry Adams <henrybruce.adams@icloud.com>
2023-04-18 15:20:09 +0100

    Got names and roles rendered to page using props.

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./EmployeeCard.scss";
 import Counter from "../Counter/Counter";
 
-const EmployeeCard = () => {
+const EmployeeCard = (props) => {
   return (
     <div className="employee-card">
       <div className="person">
-        <h3>Name</h3>
-        <h4>Role</h4>
+        <h3>{props.employee.name}</h3>
+        <h5>{props.employee.role}</h5>
       </div>
       <section>
         <Counter />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import "./Main.scss";
 import EmployeeCard from "../EmployeeCard/EmployeeCard";
+import team from "../../assets/data/team";
 
 const Main = () => {
   return (
     <div className="Main">
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
+      <EmployeeCard employee={team[0]} />
+      <EmployeeCard employee={team[1]} />
+      <EmployeeCard employee={team[2]} />
+      <EmployeeCard employee={team[3]} />
+      <EmployeeCard employee={team[4]} />
+      <EmployeeCard employee={team[5]} />
+      <EmployeeCard employee={team[6]} />
+      <EmployeeCard employee={team[7]} />
+      <EmployeeCard employee={team[8]} />
+      <EmployeeCard employee={team[9]} />
     </div>
   );
 };
